90e3ca46ff12157c43c01d72bedb73870df276d1
Wladimir Moraes Oliveira <76710345+wladimiroliveira@users.noreply.github.com>
2025-09-12 09:46:31 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home () {
+  return (
+    <div>
+      <h1>Recado para você</h1>
+      <p>Ana Paula, eu amo muito você. Sei que isso é algo simples de se falar, mas ainda assim desejo lhe dizer isso mais uma vez, amo muito você ❤️.</p>
+    </div>
+  );
+}
+
+export default Home;
