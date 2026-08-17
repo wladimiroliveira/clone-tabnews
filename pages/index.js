d438593b2b2d41c417abfb70b138702e0648f0c9
Wladimir Moraes Oliveira <76710345+wladimiroliveira@users.noreply.github.com>
2026-08-17 23:50:21 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <h1>
+      Ana Paula, eu amo você, e este é um commit de deste do{" "}
+      <a href="https://curso.dev" target="_blank">
+        curso.dev
+      </a>
+    </h1>
+  );
+}
+
+export default Home;
